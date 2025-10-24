7ee0831a6187909f283c0d9aa18421b8b412434c
Prince <prince34998@gmail.com>
2025-10-24 18:33:31 +0530

    Initial Commit

---
+++ src/main/java/com/srms/app/NotesApp.java
@@ -0,0 +1,13 @@
+package com.srms.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class NotesApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/srms/app/NotesAppTest.java
@@ -0,0 +1,38 @@
+package com.srms.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class NotesAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public NotesAppTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( NotesAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
